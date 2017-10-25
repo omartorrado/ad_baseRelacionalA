4a89ba7d98a06057fb6c710322c064cbba24beef
oracle <oracle@localhost>
2017-10-25 14:02:22 +0200

    terminado

--- src/baserelacionala/BaseRelacionalA.java
+++ src/baserelacionala/BaseRelacionalA.java
@@ -7,6 +7,8 @@ package baserelacionala;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -20,15 +22,31 @@ public class BaseRelacionalA {
     
     static Connection conn;
     static Statement st;
+    static PreparedStatement pst;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         conexion();
-        insireProduto("p1","parafusos",3);
-        insireProduto("p2","cravos",4);
-        insireProduto("p3","tachas",5);
+        //Dejo aqui escrito el metodo para limpiar la tabla durante las pruebas
+        /*
+        try {
+            st=conn.createStatement();
+            st.executeUpdate("delete from produtos");
+            } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                */
+        insireProdutoStatement("p1","parafusos",3);
+        insireProdutoStatement("p2","cravos",4);
+        insireProdutoPreparedStatement("p3","tachas",5);
+        //listaProdutosStatement();
+        actualizaPrePS("p1",6);
+        borrarFilaPS("p1");
+        listaProdutosPreparedStatement();
+        amosarFilaPS("p2");
+        borrarTodoPS("produtos");
     }
     
     public static void conexion(){
@@ -47,7 +65,7 @@ public class BaseRelacionalA {
         }
     }
     
-    public static void insireProduto(String codigo,String descricion,int prezo){
+    public static void insireProdutoStatement(String codigo,String descricion,int prezo){
         try {
             st=conn.createStatement();
             st.executeUpdate("insert into produtos values('"+codigo+"','"+descricion+"',"+prezo+")");
@@ -58,19 +76,91 @@ public class BaseRelacionalA {
         
     }
     
-    public static void listaProdutos(){
+    public static void insireProdutoPreparedStatement(String codigo,String descricion,int prezo){
+        try {
+            pst=conn.prepareStatement("insert into produtos(codigo,descricion,prezo) values(?,?,?)");
+            pst.setString(1, codigo);
+            pst.setString(2,descricion);
+            pst.setInt(3, prezo);
+            pst.executeUpdate();
+            System.out.println("Producto insertado con exito");
+        } catch (SQLException ex) {
+            System.out.println("Ha ocurrido un error, vuelva a intentarlo");
+        }
         
     }
     
-    public static void actualizaPre(){
+    public static void listaProdutosStatement(){
+        try {
+            st=conn.createStatement();
+            ResultSet rs=st.executeQuery("select Codigo,Descricion,Prezo from produtos order by Codigo");
+            while(rs.next()){
+                System.out.println("Codigo: "+rs.getString(1)+", Descrición: "+rs.getString(2)+", Prezo: "+rs.getInt(3));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void listaProdutosPreparedStatement(){
+        try {
+            pst=conn.prepareStatement("select Codigo,Descricion,Prezo from produtos order by Codigo");
+            ResultSet rs=pst.executeQuery();
+            while(rs.next()){
+                //Podemos acceder a los valores de la tabla a traves del numero de columna (empieza en 1)
+                //System.out.println("Codigo: "+rs.getString(1)+", Descrición: "+rs.getString(2)+", Prezo: "+rs.getInt(3));
+                //O podemos hacer lo mismo indicando el nombre de la columna
+                System.out.println("Codigo: "+rs.getString("Codigo")+", Descrición: "+rs.getString("Descricion")+", Prezo: "+rs.getInt("Prezo"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void actualizaPrePS(String codigo, int prezo){
+        try {
+            pst=conn.prepareStatement("update produtos set prezo=? where codigo=?");
+            pst.setInt(1, prezo);
+            pst.setString(2, codigo);
+            pst.executeUpdate();
+            System.out.println("Registro actualizado con exito");
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
-    public static void borrarFila(){
-        
+    public static void borrarFilaPS(String codigo){
+        try {
+            pst=conn.prepareStatement("delete from produtos where codigo=?");
+            pst.setString(1, codigo);
+            pst.executeUpdate();
+            System.out.println("la entrada "+codigo+" ha sido eliminada con exito");
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public static void amosarFila(){
-        
+    public static void amosarFilaPS(String codigo){
+        try {
+            pst=conn.prepareStatement("select * from produtos where codigo=?");
+            pst.setString(1, codigo);
+            ResultSet rs=pst.executeQuery();
+            while(rs.next()){
+            System.out.println("Datos de "+codigo+"-> Codigo: "+rs.getString("Codigo")+", Descrición: "+rs.getString("Descricion")+", Prezo: "+rs.getInt("Prezo"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void borrarTodoPS(String tableName){
+        try {
+            pst=conn.prepareStatement("delete from "+tableName);
+            pst.executeUpdate();
+            System.out.println("Los registros de la tabla "+tableName+" ha sido eliminada con exito");
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseRelacionalA.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
